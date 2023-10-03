4b15d410ac290bd9b82fa7950e45b838abca8e41
Builderman4 <ibrahim.syed5612@gmail.com>
2023-10-03 17:29:46 -0400

    New

---
+++ FavoriteAthlete/Athlete.swift
@@ -0,0 +1,19 @@
+//
+//  Athlete.swift
+//  FavoriteAthlete
+//
+//
+
+
+import Foundation
+
+struct Athlete {
+    var name: String
+    var age: Int
+    var league: String
+    var team: String
+    
+    var description: String {
+        return "\(name) is \(age) years old and plays for the \(team) for the \(league)."
+    }
+}
